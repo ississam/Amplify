eebd5926d0f6c6b2b122f6cc0799990818d2a01c
ississam <issam.elyazri@gmail.com>
2021-06-16 10:51:54 +0100

    change index content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello to ISsam1
+         Hello to ISsam2
         </p>
         <a
           className="App-link"
