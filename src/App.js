e3650f24566bd5aa57c072e7a8a4defa23ff0670
ississam <issam.elyazri@gmail.com>
2021-06-16 12:06:43 +0100

    change app componenet

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    Issam1
+    Issam10
         </p>
         <a
           className="App-link"
